0fdb7ffa092b8191c844c4cb5377e75d0f95841d
kouxiagu <kouxiagu@gmail.com>
2016-11-26 23:17:10 +0800

    1.0

---
+++ MinHeaps/BinaryHeap.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinHeaps
+{
+
+    /// <summary>
+    /// 二叉堆;
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BinaryHeap<T> : IEnumerable<T>
+    {
+        public BinaryHeap()
+        {
+            collection = new List<T>();
+            collection.Add(default(T));
+        }
+        public BinaryHeap(int capacity)
+        {
+            collection = new List<T>(capacity);
+            collection.Add(default(T));
+        }
+
+        /// <summary>
+        /// 根索引值;
+        /// </summary>
+        const int rootIndex = 1;
+
+        List<T> collection;
+
+        /// <summary>
+        /// 结构存在有效的元素个数;
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return collection.Count - 1;
+            }
+        }
+
+        /// <summary>
+        /// 根节点,根据排序的最大值或者最小值;
+        /// </summary>
+        public T Root
+        {
+            get
+            {
+                return collection[rootIndex];
+            }
+        }
+
+        int lastNodeIndex
+        {
+            get
+            {
+                return collection.Count - 1;
+            }
+        }
+
+        /// <summary>
+        /// 比较 other 是否置于 current 之前;
+        /// </summary>
+        protected abstract bool Compare(T current, T other);
+
+        /// <summary>
+        /// 按循序加入到数据结构;
+        /// </summary>
+        public void Add(T item)
+        {
+            collection.Add(item);
+            BubbleUp(lastNodeIndex);
+        }
+
+        /// <summary>
+        /// 输出并且移除根节点;
+        /// </summary>
+        public T Extract()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("已经不存在元素;");
+
+            T result = collection[rootIndex];
+            collection[rootIndex] = collection[lastNodeIndex];
+            BubbleDown(rootIndex);
+            collection.RemoveAt(lastNodeIndex);
+
+            return result;
+        }
+
+        public bool Contains(T item)
+        {
+            return collection.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            collection.CopyTo(array, arrayIndex);
+        }
+
+        public void Clear()
+        {
+            collection.Clear();
+            collection.Add(default(T));
+        }
+
+        void BubbleUp(int index)
+        {
+            int currentIndex = index;
+            int parentIndex = GetParentIndex(index);
+            T current = collection[currentIndex];
+
+            while (currentIndex > rootIndex)
+            {
+                if (Compare(collection[parentIndex], current))
+                {
+                    collection[currentIndex] = collection[parentIndex];
+                    currentIndex = parentIndex;
+                    parentIndex = GetParentIndex(parentIndex);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            collection[currentIndex] = current;
+        }
+
+        void BubbleDown(int index)
+        {
+            int currentIndex = index;
+            int leftChildIndex = GetLeftChildIndex(index);
+            T current = collection[currentIndex];
+
+            while (leftChildIndex <= lastNodeIndex)
+            {
+                if (leftChildIndex < lastNodeIndex && Compare(collection[leftChildIndex], collection[leftChildIndex + 1]))
+                {
+                    leftChildIndex++;
+                }
+
+                if (Compare(current ,collection[leftChildIndex]))
+                {
+                    collection[currentIndex] = collection[leftChildIndex];
+                    currentIndex = leftChildIndex;
+                    leftChildIndex = GetLeftChildIndex(leftChildIndex);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            collection[currentIndex] = current;
+        }
+
+        int GetParentIndex(int index)
+        {
+            return (int)decimal.Floor(index / 2);
+        }
+
+        int GetLeftChildIndex(int index)
+        {
+            return (2 * index);
+        }
+
+        int GetRightChildIndex(int index)
+        {
+            return (2 * index + 1);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+    }
+
+    public class MinHeap<T> : BinaryHeap<T>
+        where T : IComparable<T>
+    {
+        public MinHeap() { }
+        public MinHeap(int capacity) : base(capacity) { }
+
+        protected override bool Compare(T current, T other)
+        {
+            return other.CompareTo(current) < 0;
+        }
+    }
+
+    public class MaxHeap<T> : BinaryHeap<T>
+        where T : IComparable<T>
+    {
+        public MaxHeap() { }
+        public MaxHeap(int capacity) : base(capacity) { }
+
+        protected override bool Compare(T current, T other)
+        {
+            return other.CompareTo(current) > 0;
+        }
+    }
+
+}
--- MinHeaps/Program.cs
+++ MinHeaps/Program.cs
@@ -10,6 +10,28 @@ namespace MinHeaps
     {
         static void Main(string[] args)
         {
+            MaxHeap<int> mm = new MaxHeap<int>()
+            {
+                5,8,9,45,1,5,4,2,-2
+            };
+
+            Console.WriteLine("Root :" + mm.Root);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("移除：" + mm.Extract());
+                }
+                catch
+                {
+                    break;
+                }
+            }
         }
+
+
+
+
     }
 }
